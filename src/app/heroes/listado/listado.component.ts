67163849685c309fbd3c93bcc4f5eb21112ac766
HRR <hruiz17@hotmail.com>
2021-10-05 19:52:19 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes:string[]=['Spiderman','Iroman','Hulk','Thor','Capitan america'];
+
+  heroeBorrado:string ='';
+  borrarHeroe(){
+    this.heroeBorrado= this.heroes.shift() || '';
+  }
+
+}
